13b2b4096638b7c83d3fb4fb55317da9f0c1f984
Shankar <shankarviswanath14@gmail.com>
2026-02-03 10:21:51 +0530

    Core structure added

---
+++ src/core/planner.py
@@ -0,0 +1,49 @@
+from langchain_core.messages import HumanMessage, AIMessage
+from agents.travel_agent import agent
+from utils.custom_exception import CustomException
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class TravelPlanner:
+    
+    def __init__(self):
+        self.messages = []
+        logger.info("TravelPlanner Initialized")
+
+    def create_itinery(
+            self,
+            city: str,
+            days: int,
+            interests: list[str],
+            style: str,
+            pace: str,
+            month: str | None = None
+    ):
+        try:
+            user_prompt = f"""
+                    Plan a {days}-day trip to {city}
+
+                    Interests: {", ".join(interests)}
+                    TRavel Style: {style}
+                    Pace: {pace}
+                    Month: {month or 'Any'}
+
+                    Provide a detailed itinery
+                    """
+            
+            self.messages.append(HumanMessage(content=user_prompt))
+
+            response = agent.invoke({
+                "messages": self.messages
+
+            })
+
+            final_answer = response["messages"][-1].content
+            self.messages.append(AIMessage(content=final_answer))
+            return final_answer
+        
+        except Exception as e:
+            logger.error(f"Planner error: {e}")
+            raise CustomException(f"Failed to create itinerary: e")
